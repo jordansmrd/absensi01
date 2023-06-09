1357d6c0d45d5050abd51c4e792af5fe415f996d
Jordan <jordansumardi@hotmail.com>
2023-06-09 12:08:43 +0700

    Update api.js

--- projects/client/src/api/api.js
+++ projects/client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
- baseURL: 'http://api.absensi-01.project-kelas.pw',
+ baseURL: 'https://absensi-01.project-kelas.pw/api',
  headers: {
   ['x-secret-key']: 'rahasia'
  }
